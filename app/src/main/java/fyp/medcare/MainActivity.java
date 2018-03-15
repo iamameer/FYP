79e6f67e176aa6574f7fee9b3761a96437406fef
Ameer Sorne <ameersorne@gmail.com>
2018-03-15 10:00:50 +0800

    MainActivity skeleton

--- app/src/main/java/fyp/medcare/MainActivity.java
+++ app/src/main/java/fyp/medcare/MainActivity.java
@@ -1,28 +1,66 @@
 /*
  * MainActivity :
  *
- * Methods          :  init()         : initialize variable
- *                     setEvents()    : setting up listeners
+ * Methods          :  init()           : initialize variable
+ *                     setEvents()      : setting up listeners
+ *                     emergency_call() :
  */
 
 package fyp.medcare;
 
+import android.app.TaskStackBuilder;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Global Variables
+    private ImageView imgMainList, imgMainCheck, imgMainCall;
+    private final static String TAG = "MEDCARE";
 
     //Initialising variables
     private void init(){
-
+        imgMainList = (ImageView) findViewById(R.id.imgMainList);
+        imgMainCheck = (ImageView) findViewById(R.id.imgMainCheck);
+        imgMainCall = (ImageView) findViewById(R.id.imgMainCall);
     }
 
     //setting up methods
     private void setEvents(){
+        //starting list_activity
+        imgMainList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,list_activity.class);
+                startActivity(intent);
+            }
+        });
 
+        //starting check_activity
+        imgMainCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,check_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        //starting call()
+        imgMainCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emergency_call();
+            }
+        });
+    }
+
+    private void emergency_call(){
+        Log.d(TAG,"=MainActivity: Emergency call started");
     }
 
     //Activity Lifecycle onCreate()
@@ -33,42 +71,42 @@ public class MainActivity extends AppCompatActivity {
 
         init();
         setEvents();
-        Log.d("MedCare","=MainActivity onCreate()");
+        Log.d(TAG,"=MainActivity onCreate()");
     }
 
     //Activity Lifecycle onRestart()
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.d("MedCare","=MainActivity onRestart()");
+        Log.d(TAG,"=MainActivity onRestart()");
     }
 
     //Activity Lifecycle onResume()
     @Override
     protected void onResume(){
         super.onResume();
-        Log.d("MedCare","=MainActivity onResume()");
+        Log.d(TAG,"=MainActivity onResume()");
     }
 
     //Activity Lifecycle onPause()
     @Override
     protected void onPause(){
         super.onPause();
-        Log.d("MyTracker","=MainActivity onPause()");
+        Log.d(TAG,"=MainActivity onPause()");
     }
 
     //Activity Lifecycle onStop()
     @Override
     protected void onStop(){
         super.onStop();
-        Log.d("MedCare","=MainActivity onStop()");
+        Log.d(TAG,"=MainActivity onStop()");
     }
 
     //Activity Lifecycle onDestroy()
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.d("MedCare","=MainActivity onDestroy()");
+        Log.d(TAG,"=MainActivity onDestroy()");
     }
 
 }
